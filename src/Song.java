187c49c017044bb443e4e3f7df823c96ca6ff0c7
howarddeng <howardd2026@student.cis.edu.hk>
2024-04-12 00:28:10 +0800

    complete

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,30 @@
-public class Song {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private Set<String> listeners;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+        this.listeners = new HashSet<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(List<String> newListeners) {
+        int initialSize = listeners.size();
+        newListeners.forEach(listener -> listeners.add(listener.toLowerCase()));
+        return listeners.size() - initialSize;
+    }
 }
